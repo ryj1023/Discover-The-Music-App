7554d89eb375a8e7cd2e7702d6ba148ee8632461
Ryan Johnson <ryj1023@yahoo.com>
2016-10-22 10:40:33 -0400

    js code repositioning

--- build/app.js
+++ build/app.js
@@ -28,12 +28,12 @@ var getTourDates = function(artistName, location){
 };
 var displayTourDates = function(location, result){
 	if(result && result[0]){
-	var tourDates = result[0].artists[0].facebook_tour_dates_url;
-	$('.band-info a').attr('href', tourDates);
-	$('.band-info a').attr('target', '_blank');
-	$('.no-tours').hide();
-	$('.get-tickets').show();
-	initMap(location, result);
+		var tourDates = result[0].artists[0].facebook_tour_dates_url;
+		$('.band-info a').attr('href', tourDates);
+		$('.band-info a').attr('target', '_blank');
+		$('.no-tours').hide();
+		$('.get-tickets').show();
+		initMap(location, result);
 	}
 	else{
 		$('.loading').hide();
@@ -45,32 +45,32 @@ var displayTourDates = function(location, result){
 	$( "#artist" ).on("input", function() {
 		var input = this.value;
 	});
-	$(function() {
-		$( "#artist" ).autocomplete({
-	        minLength: 1,
-			 source: function (request, response) {
-	            $.ajax({
-	                url: 'https://api.spotify.com/v1/search',
-	                data: {
-	                    q: $("#artist").val(),
-	                    type: 'artist',
-	                    limit: 10
-	                },
-	                success: function (data) { 
-	                	response(data.artists.items)	    
-	                }
-	            });
-	        }
-	    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {       
-             return $( "<li></li>" ).click(function(){
-             		$('#artist').val(item.name);
-             		$('.band-name-input').submit()
-             })
-                .data( "item.autocomplete", item )
-                  .append( "<a class='artist-image'>" + item.name + "<br>" + '<img src=' + item.images[0].url + '>' + "</a>" )
-                .appendTo( ul );
-  };
-	 });
+$(function() {
+	$( "#artist" ).autocomplete({
+        minLength: 1,
+		 source: function (request, response) {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/search',
+                data: {
+                    q: $("#artist").val(),
+                    type: 'artist',
+                    limit: 10
+                },
+                success: function (data) { 
+                	response(data.artists.items)	    
+                }
+            });
+        }
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {       
+         return $( "<li></li>" ).click(function(){
+         		$('#artist').val(item.name);
+         		$('.band-name-input').submit()
+         })
+            .data( "item.autocomplete", item )
+              .append( "<a class='artist-image'>" + item.name + "<br>" + '<img src=' + item.images[0].url + '>' + "</a>" )
+            .appendTo( ul );
+};
+ });
 //ajax data is obtained and returned from API.
 var getMusic = function(tags){
 	$.ajax({
@@ -78,7 +78,6 @@ var getMusic = function(tags){
 		url: '//www.tastekid.com/api/similar?q=' + tags + '&callback=callBackMusic&k=227160-Discover-804XO5GB&verbose=1&type=music&info=1',
 		type: "GET",
 	})
-	//return tags;
 };
 //callback function for the API data thats finds the length of the data and diplays search result number in the counter div
 var callBackMusic = function(query){
@@ -110,33 +109,33 @@ var showMusicResults = function(music) {
 	return result;
 }
 //displays iframe and discription when each link is clicked.
-	$(document).on('click', '.band-name', function(event) {
-		$('.band-info').css('background-image', 'none')
-		$('.band-info').css('background-color', '#2196F3');
-		var _url = $(this).attr('href'),
-		description = $(this).attr('description');
-		artistName = $(this).attr('name');
-		$('#artistName').val(artistName);
-		$('.map').hide();
-		$('.no-tours').hide();
-		$('.loading').show();
-		getLocation();
-		// Prevent from opening iframe in new tab
-		event.preventDefault();
-		$('iframe, h1').css('display', 'block');
-		$('.band-info').css('display', 'block');
-		$('.band-info iframe').attr("src", _url);
-		$('.band-info h2').html(artistName);
-		$('.band-info h1').html(description);
-		readMore(description);
-		$('.band-info h2').show();
-		$('.band-info a').show();
-		$('.get-tickets').html("Get Tickets");
+$(document).on('click', '.band-name', function(event) {
+	$('.band-info').css('background-image', 'none')
+	$('.band-info').css('background-color', '#2196F3');
+	var _url = $(this).attr('href'),
+	description = $(this).attr('description');
+	artistName = $(this).attr('name');
+	$('#artistName').val(artistName);
+	$('.map').hide();
+	$('.no-tours').hide();
+	$('.loading').show();
+	getLocation();
+	// Prevent from opening iframe in new tab
+	event.preventDefault();
+	$('iframe, h1').css('display', 'block');
+	$('.band-info').css('display', 'block');
+	$('.band-info iframe').attr("src", _url);
+	$('.band-info h2').html(artistName);
+	$('.band-info h1').html(description);
+	readMore(description);
+	$('.band-info h2').show();
+	$('.band-info a').show();
+	$('.get-tickets').html("Get Tickets");
 
-		$('html, body').animate({
-		    scrollTop: $(".band-info").offset().top
-		}, 1000);
-	});
+	$('html, body').animate({
+	    scrollTop: $(".band-info").offset().top
+	}, 1000);
+});
 var readMore = function(){
 	var showChar = 200; // How many characters are shown by default
     var ellipsestext = "...";
@@ -155,7 +154,8 @@ var readMore = function(){
         if($(this).hasClass("less")) {
             $(this).removeClass("less");
             $(this).html(moretext);
-        } else {
+        } 
+        else {
             $(this).addClass("less");
             $(this).html(lesstext);
         }
@@ -167,41 +167,41 @@ var readMore = function(){
 /*Function that runs when keyword is entered and search button clicked.
 The results and counter classes are cleared and the value typed by user is stored.
 */
-	$('.band-name-input').submit(function(e) {
-		e.preventDefault();
-		//zero out results of a previous search
-		$('.display').html('');
-		$('.counter').html('');
-		// get the value of the tags the user submitted
-		var tags = $(this).find("input[name='query']").val();
-		getMusic(tags);
-		$('.results').css('padding-top', '150px');
-	}); 
+$('.band-name-input').submit(function(e) {
+	e.preventDefault();
+	//zero out results of a previous search
+	$('.display').html('');
+	$('.counter').html('');
+	// get the value of the tags the user submitted
+	var tags = $(this).find("input[name='query']").val();
+	getMusic(tags);
+	$('.results').css('padding-top', '150px');
+}); 
 var initMap = function(myLatLng, events) {
 	myLatLng = {lat: parseFloat(myLatLng.latitude), lng: parseFloat(myLatLng.longitude)};
     var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: myLatLng
   });
-  	clearOverlays();
-  for(var i = 0; i <= events.length; i++){
-		var event = events[i];
-		if(event && event.venue){
-			  	var marker = new google.maps.Marker({
-			    position: { lat: parseFloat(event.venue.latitude), lng: parseFloat(event.venue.longitude)},
-			    map: map,
-			    title: event.title
-			  });
-			  	markers.push(marker);
-  		}
-  	}		
+	clearOverlays();
+	  for(var i = 0; i <= events.length; i++){
+			var event = events[i];
+			if(event && event.venue){
+				  	var marker = new google.maps.Marker({
+				    position: { lat: parseFloat(event.venue.latitude), lng: parseFloat(event.venue.longitude)},
+				    map: map,
+				    title: event.title
+				  });
+				  	markers.push(marker);
+	  		}
+	  	}		
   			$('.loading').hide();
   			$('.map').show();
 }
 var clearOverlays = function() {
-	  for (var i = 0; i < markers.length; i++ ){
-	    markers[i].setMap(null);
-	  }
+  for (var i = 0; i < markers.length; i++ ){
+    markers[i].setMap(null);
+  }
 	  markers.length = 0;
 	}
 
