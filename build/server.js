d634c4d60b247cfca73c3e329ef00c054369c737
Ryan Johnson <ryj1023@yahoo.com>
2017-03-30 20:18:47 -0400

    added Procfile

--- build/server.js
+++ build/server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express();
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log('server is running');
 
 app.use(express.static("../build"));
